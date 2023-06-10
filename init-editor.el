HardGraphite/emacs-config

commit c256893a59aff115327988f79d4c459fa3719887

Add package `hek-usepkg`
Add a package called `hek-usepkg` to replace the package `use-package`.
Now most packages are loaded and configured with `hek-usepkg`.

init-editor.el
@@ -2,23 +2,8 @@
 
 ;;;;; Mode line ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;;; doom-modeline, https://github.com/seagle0128/doom-modeline
-;; (use-package doom-modeline
-;;   :init
-;;   (doom-modeline-mode 1)
-;;   :config
-;;   (setq doom-modeline-continuous-word-count-modes '()
-;;         doom-modeline-indent-info t
-;;         doom-modeline-gnus nil
-;;         doom-modeline-irc nil
-;;         doom-modeline-battery nil
-;;         doom-modeline-time nil
-;;         doom-modeline-env-version nil
-;;         doom-modeline-always-visible-segments '()
-;;         ))
-
-(use-package hek-modeline
-  :ensure nil ;; my code
+(hek-usepkg hek-modeline
+  :from local
   :config
   (hek-modeline-mode 1))
 
@@ -62,15 +47,15 @@
 (add-hook 'text-mode-hook #'display-fill-column-indicator-mode)
 
 ;;; Indentation indicator.
-(use-package highlight-indent-guides
-  :defer t
+(hek-usepkg highlight-indent-guides
+  :from package
   :config
   (setq highlight-indent-guides-method 'bitmap ;; Emacs with xpm support is required.
         highlight-indent-guides-bitmap-function #'highlight-indent-guides--bitmap-line
         highlight-indent-guides-responsive 'top
         highlight-indent-guides-delay 0.5)
   :hook
-  ((prog-mode tex-mode) . highlight-indent-guides-mode))
+  ((prog-mode-hook tex-mode-hook) . highlight-indent-guides-mode))
 
 
 ;;;;; Brackets and pairs ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -90,8 +75,8 @@
 (set-face-background 'show-paren-match 'unspecified)
 
 ;;; rainbow-delimiters, https://github.com/Fanael/rainbow-delimiters
-(use-package rainbow-delimiters
-  :defer t
+(hek-usepkg rainbow-delimiters
+  :from package
   :config
   (custom-set-faces
    '(rainbow-delimiters-depth-1-face ((t (:foreground "dark orange"))))
@@ -103,7 +88,7 @@
    '(rainbow-delimiters-depth-7-face ((t (:foreground "spring green"))))
    '(rainbow-delimiters-depth-8-face ((t (:foreground "sienna1")))))
   :hook
-  (emacs-lisp-mode . rainbow-delimiters-mode))
+  (emacs-lisp-mode-hook . rainbow-delimiters-mode))
 
 ;;;;; Whitespace and Coding style ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -123,7 +108,8 @@
               (delete-trailing-whitespace))))
 
 ;;; hungry-delete, https://github.com/nflath/hungry-delete
-(use-package hungry-delete
+(hek-usepkg hungry-delete
+  :from package
   :init
   (global-hungry-delete-mode)
   :config
@@ -186,7 +172,7 @@
 (setq c-default-style '((java-mode . "java") (other . "my")))
 
 ;;; editorconfig, https://github.com/editorconfig/editorconfig-emacs.
-(use-package editorconfig
+(hek-usepkg editorconfig
   :init (editorconfig-mode 1))
 
 ;;;;; Spell / grammar check ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -218,7 +204,8 @@
 ;;;;; Search and navigation ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;;; consult, https://github.com/minad/consult
-(use-package consult
+(hek-usepkg consult
+  :from package
   :init
   (setq xref-show-xrefs-function #'consult-xref
         xref-show-definitions-function #'consult-xref)
@@ -246,22 +233,24 @@
    ))
 
 ;;; avy, https://github.com/abo-abo/avy
-(use-package avy
+(hek-usepkg avy
+  :from package
   :config
   (setq avy-style 'at-full
         avy-highlight-first t)
   ;; :bind
-  ;; ("C-'" . avy-goto-char-2)
-  ;; ("C-\"" . avy-goto-word-1)
+  ;; (("C-'" . avy-goto-char-2)
+  ;;  ("C-\"" . avy-goto-word-1))
   )
 
 ;;; ace-window, https://github.com/abo-abo/ace-window
-(use-package ace-window
+(hek-usepkg ace-window
+  :from package
   :config
   (setq aw-minibuffer-flag t
         aw-ignore-current  t)
   :bind
-  ("C-x o" . ace-window))
+  (("C-x o" . ace-window)))
 
 ;;; Xref search
 (setq xref-search-program 'ripgrep)
@@ -269,8 +258,8 @@
 
 ;;;;; Project ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(use-package project
-  :ensure nil ;; built-in
+(hek-usepkg project
+  :from builtin
   :config
   ;; --- hacks ---
   ;; -- add a project find function
@@ -288,20 +277,6 @@
   (add-to-list 'project-find-functions #'+project-root-marker-file-find-root)
   ;; --- hacks end ---
   )
-
-;; ;;; Projectile, https://github.com/bbatsov/projectile
-;; (use-package projectile
-;;   :init
-;;   (projectile-mode 1)
-;;   :config
-;;   (setq projectile-require-project-root t
-;;         projectile-auto-discover nil
-;;         projectile-completion-system 'default ;; vertico
-;;         projectile-sort-order 'recently-active
-;;         projectile-switch-project-action #'projectile-dired
-;;         projectile-mode-line-prefix "PROJECT")
-;;   :bind
-;;   ("C-x p" . projectile-command-map))
 
 
 ;;;;; Compilations ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;

init-theme.el
@@ -24,8 +24,9 @@
 ;; (+my-modeline-font-setup) ;; Call after Emacs theme is loaded.
 
 ;;; Ligatures.
-(use-package hek-ligature
-  :ensure nil ;; my code
+(hek-usepkg hek-ligature
+  :from local
+  :defer 0
   :init
   (setq hek-ligature-table
     '((prog-mode
@@ -47,15 +48,12 @@
         (?# . ".\\(?:#+}\\|[([{]\\)")
         (?_ . "._+"))))
   :hook
-  ((prog-mode . hek-ligature-mode)))
-
-;; ;;; all-the-icons, https://github.com/domtronn/all-the-icons.el
-;; (use-package all-the-icons)
+  (prog-mode-hook . hek-ligature-mode))
 
 ;;; nerd-icons :: use Nerd Font icons, an alternative to `all-the-icons'
 ;;; https://github.com/rainstormstudio/nerd-icons.el
-(use-package nerd-icons
-  :defer t
+(hek-usepkg nerd-icons
+  :from package
   :init
   (setq nerd-icons-font-family *my-nerd-font-family*))
 
@@ -63,7 +61,8 @@
 ;;;;; Theme ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;;; doom-themes, https://github.com/doomemacs/themes
-(use-package doom-themes
+(hek-usepkg doom-themes
+  :from package
   :init
   (setq doom-themes-enable-bold t
         doom-themes-enable-italic t)
@@ -76,8 +75,8 @@
   )
 
 ;; ;;; Dark/light themes.
-;; (use-package hek-yinyang
-;;   :ensure nil ;; my code
+;; (hek-usepkg hek-yinyang
+;;   :from local
 ;;   :init
 ;;   (setq hek-yinyang-dark-theme  'doom-vibrant
 ;;         hek-yinyang-light-theme 'doom-one-light
@@ -90,7 +89,7 @@
 
 ;;; Solaire mode :: different background darkness between special / file buffers
 ;;; https://github.com/hlissner/emacs-solaire-mode
-(use-package solaire-mode
+(hek-usepkg solaire-mode
   :init
   (solaire-global-mode 1))
 

Makefile
@@ -21,8 +21,7 @@ debug:
 
 .PHONY: get-pkgs
 get-pkgs:
-	@emacs --install-packages --eval '(switch-to-buffer "*Messages*")'
-	# See "Package management" section in init-system.el
+	@emacs --install-packages --debug-init --eval '(switch-to-buffer "*Messages*")'
 
 .PHONY: ls-inits
 ls-inits:
@@ -38,4 +37,4 @@ ls-heads:
 .PHONY: ls-pkgs
 ls-pkgs:
 	@grep ${GREP_FLAGS} --perl-regexp \
-		'\(use-package\s+\K([a-zA-Z0-9\-_]+)' ${INIT_XXX_FILES}
+		'\(hek-usepkg\s+\K([a-zA-Z0-9\-_]+)' ${INIT_XXX_FILES}

init-complete.el
@@ -5,10 +5,11 @@
 ;;;;; Completion algorithm and category ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;;; orderless, https://github.com/oantolin/orderless
-(use-package orderless
+(hek-usepkg orderless
+  :from package
   :init
-  ;(setq orderless-style-dispatchers '(+orderless-consult-dispatch orderless-affix-dispatch)
-  ;      orderless-component-separator #'orderless-escapable-split-on-space)
+  ;; (setq orderless-style-dispatchers '(+orderless-consult-dispatch orderless-affix-dispatch)
+  ;;      orderless-component-separator #'orderless-escapable-split-on-space)
   )
 
 (dolist (name '(emacs21 emacs22 substring initials))
@@ -22,72 +23,34 @@
 ;;;;; Minibuffer completion front-end ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;;; vertico, https://github.com/minad/vertico
-(use-package vertico
+(hek-usepkg vertico
+  :from package
   :init
   (setq vertico-scroll-margin 2
         vertico-count 10
         vertico-cycle t)
   (vertico-mode 1)
   :bind
-  (:map vertico-map
-        ("M-j" . vertico-next)
-        ("M-k" . vertico-previous)))
-(use-package vertico-directory
+  (vertico-map
+   ("M-j" . vertico-next)
+   ("M-k" . vertico-previous)))
+(hek-usepkg vertico-directory
   :after vertico
-  :ensure nil
-  :bind (:map vertico-map
-         ("RET" . vertico-directory-enter)))
-;; (use-package vertico-indexed
-;;   :after vertico
-;;   :ensure nil
-;;   :init
-;;   (vertico-indexed-mode 1))
-;; (use-package vertico-reverse
-;;   :after vertico
-;;   :ensure nil
-;;   :init
-;;   (vertico-reverse-mode 1)
-;;   :bind
-;;   (:map vertico-reverse-map
-;;         ("M-k" . vertico-next)
-;;         ("M-j" . vertico-previous)))
+  :bind
+  (vertico-map
+   ("RET" . vertico-directory-enter)))
 
 ;;; marginalia, https://github.com/minad/marginalia
-(use-package marginalia
+(hek-usepkg marginalia
+  :from package
   :init
   (marginalia-mode 1))
 
 ;;;;; Code (in-buffer) completion ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; ;;; company-mode, https://github.com/company-mode/company-mode
-;; (use-package company
-;;   :init
-;;   (global-company-mode 1)
-;;   :config
-;;   (set-face-attribute 'company-tooltip nil
-;;     :family *my-term-font-family* :height *my-term-font-height*)
-;;   (setq company-idle-delay 0.1
-;;         company-minimum-prefix-length 1
-;;         completion-ignore-case t
-;;         company-tooltip-align-annotations t
-;;         company-frontends
-;;           '(company-pseudo-tooltip-frontend
-;;             company-echo-metadata-frontend)
-;;         company-backends
-;;           '((company-capf :with company-yasnippet)
-;;             company-dabbrev)
-;;         company-dabbrev-other-buffers nil
-;;         company-dabbrev-ignore-case nil
-;;         company-dabbrev-downcase nil)
-;;   :bind
-;;   (:map company-active-map
-;;         ("C-n" . company-select-next)
-;;         ("M-j" . company-select-next)
-;;         ("C-p" . company-select-previous)
-;;         ("M-k" . company-select-previous)))
-
 ;;; corfu, https://github.com/minad/corfu
-(use-package corfu
+(hek-usepkg corfu
+  :from package
   :init
   (global-corfu-mode 1)
   :config
@@ -109,16 +72,17 @@
   (add-to-list 'corfu-margin-formatters
                #'hek-corfu-nerd-icons-margin-formatter)
   :bind
-  (:map corfu-map
-        ("M-<escape>" . corfu-quit)
-        ("<escape>" . (lambda () (interactive) (meow-insert-exit) (corfu-quit))) ;; for meow-mode
-        ("M-j" . corfu-next)
-        ("M-k" . corfu-previous)
-        ("M-v" . corfu-scroll-up)
-        ("M-V" . corfu-scroll-down)))
+  (corfu-map
+   ("M-<escape>" . corfu-quit)
+   ("<escape>" . (lambda () (interactive) (meow-insert-exit) (corfu-quit))) ;; for meow-mode
+   ("M-j" . corfu-next)
+   ("M-k" . corfu-previous)
+   ("M-v" . corfu-scroll-up)
+   ("M-V" . corfu-scroll-down)))
 
 ;;; cape, https://github.com/minad/cape
-(use-package cape
+(hek-usepkg cape
+  :from package
   :init
   (dolist (capf
            '(cape-dabbrev
@@ -149,37 +113,27 @@
 
 ;;;;; Code templates ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; ;;; YASnippet, https://github.com/joaotavora/yasnippet
-;; (use-package yasnippet
-;;   :defer t
-;;   :init
-;;   ;; I really don't like yasnippet's way of storing snippets.
-;;   (setq yas-snippet-dirs nil)
-;;   ;; ;; So, let me do it myself.
-;;   ;; (let ((my-snippet-dir (concat *my-emacs-conf-dir* "snippets")))
-;;   ;;   (load (expand-file-name "snippet-util" my-snippet-dir))
-;;   ;;   (snippet-util-load-to-yasnippet my-snippet-dir))
-;;   :hook
-;;   ((prog-mode tex-mode) . yas-minor-mode))
-
 ;;; TempEl :: code templates (snippets)
 ;;; https://github.com/minad/tempel
-(use-package tempel
-  :defer t
+(hek-usepkg tempel
+  :from package
   :config
   (setq hek-tempel-snippets-dir (concat *my-emacs-conf-dir* "snippets/"))
   (require 'hek-tempel-snippets)
   (push #'hek-tempel-snippets tempel-template-sources)
   :bind
-  (("M-=" . tempel-insert)
-   :map tempel-map
-        ("S-TAB" . tempel-previous)
-        ("TAB" . tempel-next)
-        ("M-<escape>" . tempel-done)))
+  (("M-=" . tempel-insert))
+  :bind~
+  (tempel-map
+   ("S-TAB" . tempel-previous)
+   ("TAB" . tempel-next)
+   ("M-<escape>" . tempel-done)))
 
 ;;; lsp-snippet :: bridge between TempEl and Eglot
 ;;; https://github.com/svaante/lsp-snippet
-(use-package lsp-snippet-tempel
-  :ensure nil ;; local
+(hek-usepkg lsp-snippet-tempel
+  ;; :from package-vc "https://github.com/svaante/lsp-snippet"
+  :from local
+  :after eglot
   :config
-    (lsp-snippet-tempel-eglot-init))
+  (eglot lsp-snippet-tempel-eglot-init))

init-keymaps.el
@@ -180,15 +180,9 @@
 
 ;;;;; Modal editing ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; ;;; evil, https://github.com/emacs-evil/evil
-;; (use-package evil
-;;   :preface
-;;   (setq evil-undo-system 'undo-redo)
-;;   :init
-;;   (evil-mode 1))
-
 ;;; Meow, https://github.com/meow-edit/meow
-(use-package meow
+(hek-usepkg meow
+  :from package
   :init
   ;; --- pre-start configs ---
   (setq meow-use-cursor-position-hack t
@@ -389,7 +383,8 @@
 ;;;;; Keymap tools ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;;; which-key, https://github.com/justbur/emacs-which-key
-(use-package which-key
+(hek-usepkg which-key
+  :from package
   :init
   (which-key-mode 1)
   :config

init-langs.el
@@ -3,9 +3,8 @@
 ;;;;; LSP (language server protocol) ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;;; Eglot, https://github.com/joaotavora/eglot
-(use-package eglot
-  ;; :ensure nil ;; built-in
-  :defer t
+(hek-usepkg eglot
+  :from package ;; FIXME: builtin after Emacs 29
   :init
   (setq eglot-autoshutdown t
         eglot-events-buffer-size 0)
@@ -16,41 +15,41 @@
       (lua-mode "lua-language-server")
       ((tex-mode bibtex-mode) "texlab")))
   :hook
-  ((c-mode c++-mode
-    python-mode
-    lua-mode
-    tex-mode bibtex-mode)
+  ((c-mode-hook c++-mode-hook
+    python-mode-hook
+    lua-mode-hook
+    tex-mode-hook bibtex-mode-hook)
    . eglot-ensure))
 
 ;;;;; Tree-sitter ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;;; ELisp Tree-sitter, https://github.com/emacs-tree-sitter/elisp-tree-sitter
-(use-package tree-sitter
-  ;; :ensure nil ;; built-in
-  :defer t
+(hek-usepkg tree-sitter
+  :from package
   :hook
-  ((sh-mode
-    ;; c-mode c++-mode ;; <-- clangd provides highlight info
-    ;; csharp-mode
-    css-mode
-    ;; go-mode
-    ;; haskell-mode
-    html-mode
-    java-mode
-    javascript-mode
-    ;; json-mode
-    ;; julia-mode
-    ;; lua-mode
-    ;; php-mode
-    python-mode
-    rust-mode
-    ;; toml-mode
-    ;; yaml-mode
-    verilog-mode
-    markdown-mode)
+  ((sh-mode-hook
+    ;; c-mode-hook c++-mode-hook ;; <-- clangd provides highlight info
+    ;; csharp-mode-hook
+    css-mode-hook
+    ;; go-mode-hook
+    ;; haskell-mode-hook
+    html-mode-hook
+    java-mode-hook
+    javascript-mode-hook
+    ;; json-mode-hook
+    ;; julia-mode-hook
+    ;; lua-mode-hook
+    ;; php-mode-hook
+    python-mode-hook
+    rust-mode-hook
+    ;; toml-mode-hook
+    ;; yaml-mode-hook
+    verilog-mode-hook
+    markdown-mode-hook)
    . tree-sitter-mode)
   (tree-sitter-after-on . tree-sitter-hl-mode))
-(use-package tree-sitter-langs
+(hek-usepkg tree-sitter-langs
+  :from package
   :after tree-sitter)
 ;; See `https://github.com/nvim-treesitter/nvim-treesitter/tree/master/queries'
 ;; for query definitions from Nvim's tree sitter plugin.
@@ -59,14 +58,16 @@
 
 ;; --- Markdown ---
 ;; Markdown mode, https://github.com/jrblevin/markdown-mode
-(use-package markdown-mode
+(hek-usepkg markdown-mode
+  :from package
   :defer t
-  :init
-  (setq markdown-command '("pandoc" "--from=markdown" "--to=html5" "--standalone" "--mathjax")))
+  :config
+  (setq markdown-command
+        '("pandoc" "--from=markdown" "--to=html5" "--standalone" "--mathjax")))
 
 ;; --- Verilog HDL / SystemVerilog ---
-(use-package verilog-mode
-  :ensure nil ;; built-in
+(hek-usepkg verilog-mode
+  :from builtin
   :defer t
   :config
   (setq verilog-align-ifelse t
@@ -90,8 +91,5 @@
         )
   ;; (setq verilog-linter "verilator -–lint-only -Wall")
   (require 'hek-flymake-verilator)
-  (defun +verilog-mode-setup ()
-    (hek-flymake-verilator-setup)
-    (flymake-mode 1))
-  :hook
-  ((verilog-mode . +verilog-mode-setup)))
+  (hek-flymake-verilator-setup)
+  (flymake-mode 1))

init-system.el
@@ -29,21 +29,17 @@
 (setq package-quickstart t)
 (package-initialize)
 
-;;; Prepare `use-package'.
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
+;;; Prepare `hek-usepkg', which helps to setup packages.
 (eval-when-compile
-  (require 'use-package)
-  (setq use-package-expand-minimally t))
+  (require 'hek-usepkg))
 
 ;;; Install packages.
 (let ((package-install-switch "--install-packages"))
   (when (member package-install-switch command-line-args)
-    (setq command-line-args (delete package-install-switch command-line-args))
-    (setq use-package-always-ensure t
-          use-package-verbose t
-          native-comp-async-query-on-exit t)
+    (setq command-line-args (delete package-install-switch command-line-args)) ;; Remove the switch.
+    (setq hek-usepkg-ensure t
+          hek-usepkg-debug t)
+    (setq native-comp-async-query-on-exit t)
     (when package-quickstart
       (add-hook 'kill-emacs-hook #'package-quickstart-refresh))
     (package-refresh-contents)))
@@ -98,11 +94,12 @@
 (setq redisplay-skip-fontification-on-input t)
 
 ;;; Clever carbage collection.
-(use-package gcmh
-  :defer t
+(hek-usepkg gcmh
+  :from package
+  :init
+  (defun +gcmh-setup ()
+    (setq gcmh-idle-delay 10
+          gcmh-high-cons-threshold #x1000000) ;; 16 MiB
+    (gcmh-mode 1))
   :hook
-  (emacs-startup
-   . (lambda ()
-       (setq gcmh-idle-delay 10
-             gcmh-high-cons-threshold #x1000000) ;; 16 MiB
-       (gcmh-mode 1))))
+  (emacs-startup-hook . +gcmh-setup))

init-tools.el
@@ -2,49 +2,10 @@
 
 ;;;;; Dashboard ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; ;;; emacs-dashboard, https://github.com/emacs-dashboard/emacs-dashboard
-;; (use-package dashboard
-;;   :init
-;;   (dashboard-setup-startup-hook)
-;;   (when (daemonp)
-;;     (setq initial-buffer-choice
-;;       (lambda ()
-;;         (dashboard-refresh-buffer)
-;;         ;;(get-buffer-create "*dashboard*")
-;;         )))
-;;   :config
-;;   (setq dashboard-display-icons-p t
-;;         dashboard-banner-logo-title nil
-;;         dashboard-items
-;;           '((recents . 5) (projects . 5) (bookmarks . 5) (agenda . 5) (registers . 5))
-;;         dashboard-projects-backend 'project-el ;; or projectile if installed
-;;         dashboard-center-content  t
-;;         dashboard-set-init-info   nil
-;;         dashboard-init-info       nil
-;;         dashboard-set-footer      t
-;;         dashboard-footer          nil
-;;         ;;dashboard-footer-icon     nil
-;;         dashboard-footer-messages nil)
-;;   (defun dashboard-display-icons-p ()
-;;     t)
-;;   (defun dashboard-insert-footer ()
-;;     (insert "\n")
-;;     (dashboard-insert-center
-;;       dashboard-footer-icon
-;;       " "
-;;       (propertize (concat "GNU Emacs " emacs-version (when (daemonp) " (client)"))
-;;         'face 'dashboard-footer)
-;;       "\n"))
-;;   (set-face-attribute 'dashboard-banner-logo-title nil
-;;     :family *my-text-font-family*)
-;;   (set-face-attribute 'dashboard-heading nil
-;;     :family *my-text-font-family* :height 170)
-;;   )
-
-(use-package hek-homepage
-  :ensure nil ;; my code
-  :if (or (not command-line-args)
-          (not (cdr command-line-args))) ;; No extra arguments.
+(hek-usepkg hek-homepage
+  :from local
+  :when (or (not command-line-args)
+            (not (cdr command-line-args))) ;; No extra arguments.
   :init
   (setq initial-major-mode #'fundamental-mode
         inhibit-startup-screen t)
@@ -73,34 +34,29 @@
 
 ;;;;; Directory view ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; ;;; all-the-icons-dired, https://github.com/jtbm37/all-the-icons-dired
-;; (use-package all-the-icons-dired
-;;   :after all-the-icons
-;;   :hook (dired-mode . all-the-icons-dired-mode))
-
 ;;; dirvish :: an improved version of `dired'
 ;;; https://github.com/alexluigit/dirvish
-(use-package dirvish
-  :defer t
+(hek-usepkg dirvish
+  :from package
   :config
   (setq dirvish-use-header-line 'global
         dirvish-attributes '(vc-state subtree-state nerd-icons file-time file-size)
         dirvish-subtree-state-style 'nerd
         dirvish-subtree-always-show-state t)
-  :bind
-  (:map dirvish-mode-map
-        ("TAB" . dirvish-subtree-toggle)
-        ("`" . dirvish-layout-toggle)
-        ("/" . dired-isearch-filenames))
+  :bind~
+  (dirvish-mode-map
+   ("TAB" . dirvish-subtree-toggle)
+   ("`" . dirvish-layout-toggle)
+   ("/" . dired-isearch-filenames))
   :hook
-  (after-init . dirvish-override-dired-mode))
+  (after-init-hook . dirvish-override-dired-mode))
 
 
 ;;;;; Terminal ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Emacs-libvterm, https://github.com/akermu/emacs-libvterm
-(use-package vterm
-  :defer t
+(hek-usepkg vterm
+  :from package
   :init
   (defun +shell-vterm (&optional buffer-name)
     (if (stringp buffer-name)
@@ -116,29 +72,30 @@
     (buffer-face-set :family *my-term-font-family* :height *my-term-font-height*))
   :bind
   (("<f12>" . vterm-other-window))
-  :bind
-  (:map vterm-mode-map
+  :bind~
+  (vterm-mode-map
    ("C-<escape>" . vterm-send-next-key)
    ("C-y" . vterm-copy-mode)
    ("C-x" . vterm--self-insert)
    ("C-c" . vterm--self-insert))
   :hook
-  (vterm-mode . +vterm-local-init))
+  (vterm-mode-hook . +vterm-local-init))
 
 
 ;;;;; Version control systems ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Magit, https://github.com/magit/magit
-(use-package magit
+(hek-usepkg magit
+  :from package
   :defer t
   :config
   (setq magit-save-repository-buffers nil))
 
 ;; diff-hl, https://github.com/dgutov/diff-hl
-(use-package diff-hl
-  :defer t
+(hek-usepkg diff-hl
+  :from package
   :hook
-  ;; (dired-mode . diff-hl-dired-mode) ;; use `dirvish-vc' instead
-  ((prog-mode tex-mode markdown-mode) . diff-hl-mode)
-  (magit-pre-refresh . diff-hl-magit-pre-refresh)
-  (magit-post-refresh . diff-hl-magit-post-refresh))
+  ;; (dired-mode-hook . diff-hl-dired-mode) ;; use `dirvish-vc' instead
+  ((prog-mode-hook tex-mode-hook markdown-mode) . diff-hl-mode)
+  (magit-pre-refresh-hook . diff-hl-magit-pre-refresh)
+  (magit-post-refresh-hook . diff-hl-magit-post-refresh))

commit 257c36631ee4e9c327c91575f2c425f749ff074e

Fix problems introduced in commit c256893

init-editor.el
@@ -173,6 +173,7 @@
 
 ;;; editorconfig, https://github.com/editorconfig/editorconfig-emacs.
 (hek-usepkg editorconfig
+  :from package
   :init (editorconfig-mode 1))
 
 ;;;;; Spell / grammar check ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;

init-theme.el
@@ -26,7 +26,6 @@
 ;;; Ligatures.
 (hek-usepkg hek-ligature
   :from local
-  :defer 0
   :init
   (setq hek-ligature-table
     '((prog-mode
@@ -47,6 +46,7 @@
         (?. . ".\\.+")
         (?# . ".\\(?:#+}\\|[([{]\\)")
         (?_ . "._+"))))
+  (autoload 'hek-ligature-mode "hek-ligature")
   :hook
   (prog-mode-hook . hek-ligature-mode))
 
@@ -63,11 +63,10 @@
 ;;; doom-themes, https://github.com/doomemacs/themes
 (hek-usepkg doom-themes
   :from package
-  :init
+  :config
   (setq doom-themes-enable-bold t
         doom-themes-enable-italic t)
   (load-theme 'doom-one t)
-  :config
   ;; (doom-themes-visual-bell-config)
   ;; (doom-themes-neotree-config) ;; for neotree
   ;; (setq doom-themes-treemacs-theme "doom-atom") (doom-themes-treemacs-config);; for treemacs
@@ -90,7 +89,8 @@
 ;;; Solaire mode :: different background darkness between special / file buffers
 ;;; https://github.com/hlissner/emacs-solaire-mode
 (hek-usepkg solaire-mode
-  :init
+  :from package
+  :config
   (solaire-global-mode 1))
 
 (+my-modeline-font-setup)