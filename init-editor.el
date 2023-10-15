88ab66db241aedee0ebe7dc1709e323c375df170
HardGraphite <jodieydchen@gmail.com>
2023-10-15 11:18:10 +0800

    Add package `hek-hl-indent`

--- init-editor.el
+++ init-editor.el
@@ -54,6 +54,13 @@
 (add-hook 'prog-mode-hook #'display-fill-column-indicator-mode)
 (add-hook 'text-mode-hook #'display-fill-column-indicator-mode)
 
+;;; Indentation indicator.
+(add-hook
+ 'prog-mode-hook
+ (lambda ()
+   (unless indent-tabs-mode
+     (hek-hl-indent-mode))))
+
 
 ;;;;; Brackets and pairs ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
