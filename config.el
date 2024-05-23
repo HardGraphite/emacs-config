ba71336f327fccf83ddcbf8ea1f22614d957079a
HardGraphite <jodieydchen@gmail.com>
2024-05-23 16:00:09 +0800

    Use treesit modes for C/C++ editing

--- config.el
+++ config.el
@@ -866,6 +866,8 @@
   (setq treesit-language-source-alist
         '(;; (bash "https://github.com/tree-sitter/tree-sitter-bash")
           ;; (cmake "https://github.com/uyha/tree-sitter-cmake")
+          (c "https://github.com/tree-sitter/tree-sitter-c")
+          (cpp "https://github.com/tree-sitter/tree-sitter-cpp")
           (json "https://github.com/tree-sitter/tree-sitter-json")
           (python "https://github.com/tree-sitter/tree-sitter-python")
           (toml "https://github.com/tree-sitter/tree-sitter-toml")
@@ -877,10 +879,12 @@
         (treesit-install-language-grammar lang))))
   :init
   (setq major-mode-remap-alist
-        '((js-json-mode . json-ts-mode)
+        '((c-mode . c-ts-mode)
+          (c++-mode . c++-ts-mode)
+          (c-or-c++-mode . c-or-c++-ts-mode)
+          (js-json-mode . json-ts-mode)
           (python-mode . python-ts-mode)
-          (sh-mode . bash-ts-mode)))
-  )
+          (sh-mode . bash-ts-mode))))
 
 
 ;;;;;** C-like languages
@@ -927,6 +931,83 @@
         '((java-mode . "java")
           (awk-mode  . "awk")
           (other     . "my")))
+  )
+
+(with-eval-after-load "c-ts-mode"
+
+  (defconst +c-ts-mode-indent-style
+    ;; Adapted from function `c-ts-mode--indent-styles'.
+    '((c-ts-mode--for-each-tail-body-matcher prev-line c-ts-mode-indent-offset)
+      (c-ts-mode--else-heuristic prev-line c-ts-mode-indent-offset)
+      ((parent-is "translation_unit") column-0 0)
+      ((query "(ERROR (ERROR)) @indent") prev-line 0)
+      ((node-is ")") parent-bol 0)
+      ((node-is "]") parent-bol 0)
+      ((node-is "else") parent-bol 0)
+      ((node-is "case") parent-bol 0)
+      ((node-is "preproc_arg") no-indent)
+      ((and (parent-is "comment") c-ts-common-looking-at-star) c-ts-common-comment-start-after-first-star -1)
+      (c-ts-common-comment-2nd-line-matcher c-ts-common-comment-2nd-line-anchor 1)
+      ((parent-is "comment") prev-adaptive-prefix 0)
+      ((node-is "labeled_statement") standalone-parent 0)
+      ((parent-is "labeled_statement") c-ts-mode--standalone-grandparent c-ts-mode-indent-offset)
+      ((node-is "preproc") column-0 0)
+      ((node-is "#endif") column-0 0)
+      ((match "preproc_call" "compound_statement") column-0 0)
+      ((n-p-gp nil "preproc" "translation_unit") column-0 0)
+      ((and no-node (parent-is "\\(?:\n\\|preproc\\)")) c-ts-mode--standalone-parent-skip-preproc c-ts-mode--preproc-offset)
+      ((match nil "preproc_\\(?:\\(?:el\\)?if\\)" nil 3 3) c-ts-mode--standalone-parent-skip-preproc c-ts-mode-indent-offset)
+      ((match nil "preproc_ifdef" nil 2 2) c-ts-mode--standalone-parent-skip-preproc c-ts-mode-indent-offset)
+      ((match nil "preproc_else" nil 1 1) c-ts-mode--standalone-parent-skip-preproc c-ts-mode-indent-offset)
+      ((parent-is "preproc") c-ts-mode--anchor-prev-sibling 0)
+      ((parent-is "function_definition") parent-bol 0)
+      ((parent-is "pointer_declarator") parent-bol 0)
+      ((parent-is "\\`declaration\\'") parent-bol 0)
+      ((parent-is "conditional_expression") parent-bol c-ts-mode-indent-offset)
+      ((parent-is "assignment_expression") parent-bol c-ts-mode-indent-offset)
+      ((parent-is "concatenated_string") parent-bol c-ts-mode-indent-offset)
+      ((parent-is "comma_expression") parent-bol c-ts-mode-indent-offset)
+      ((parent-is "init_declarator") parent-bol c-ts-mode-indent-offset)
+      ((parent-is "parenthesized_expression") parent-bol c-ts-mode-indent-offset)
+      ((parent-is "argument_list") parent-bol c-ts-mode-indent-offset)
+      ((parent-is "parameter_list") parent-bol c-ts-mode-indent-offset)
+      ((parent-is "binary_expression") parent-bol c-ts-mode-indent-offset)
+      ((query "(for_statement initializer: (_) @indent)") parent-bol c-ts-mode-indent-offset)
+      ((query "(for_statement condition: (_) @indent)") parent-bol c-ts-mode-indent-offset)
+      ((query "(for_statement update: (_) @indent)") parent-bol c-ts-mode-indent-offset)
+      ((query "(call_expression arguments: (_) @indent)") parent c-ts-mode-indent-offset)
+      ((parent-is "call_expression") parent-bol c-ts-mode-indent-offset)
+      ((node-is "}") standalone-parent 0)
+      ((n-p-gp nil nil "namespace_definition") grand-parent 0) ; C++
+      ((node-is "access_specifier") parent-bol 0) ; C++
+      ((parent-is "declaration_list") parent-bol c-ts-mode-indent-offset) ; C++
+      ((match nil "initializer_list" nil 1 1) parent-bol c-ts-mode-indent-offset)
+      ((parent-is "initializer_list") c-ts-mode--anchor-prev-sibling 0)
+      ((match nil "enumerator_list" nil 1 1) standalone-parent c-ts-mode-indent-offset)
+      ((parent-is "enumerator_list") c-ts-mode--anchor-prev-sibling 0)
+      ((match nil "field_declaration_list" nil 1 1) standalone-parent c-ts-mode-indent-offset)
+      ((parent-is "field_declaration_list") c-ts-mode--anchor-prev-sibling 0)
+      ((or (and (parent-is "compound_statement") c-ts-mode--first-sibling) (match null "compound_statement")) standalone-parent c-ts-mode-indent-offset)
+      ((parent-is "compound_statement") c-ts-mode--anchor-prev-sibling 0)
+      ((node-is "compound_statement") standalone-parent c-ts-mode-indent-offset)
+      ((match "expression_statement" nil "body") standalone-parent c-ts-mode-indent-offset)
+      ((parent-is "if_statement") standalone-parent c-ts-mode-indent-offset)
+      ((parent-is "else_clause") standalone-parent c-ts-mode-indent-offset)
+      ((parent-is "for_statement") standalone-parent c-ts-mode-indent-offset)
+      ((match "while" "do_statement") parent-bol 0) ; (do_statement "while")
+      ((parent-is "while_statement") standalone-parent c-ts-mode-indent-offset)
+      ((parent-is "do_statement") standalone-parent c-ts-mode-indent-offset)
+      ((parent-is "case_statement") standalone-parent c-ts-mode-indent-offset)
+      ((node-is "field_initializer_list") parent-bol c-ts-mode-indent-offset) ; C++
+      (no-node prev-line 0)
+      ))
+
+  (defun +c-ts-mode-indent-style ()
+    +c-ts-mode-indent-style)
+
+  (setq c-ts-mode-indent-offset 4
+        c-ts-mode-indent-style #'+c-ts-mode-indent-style)
+
   )
 
 ;;;;;** CMake
